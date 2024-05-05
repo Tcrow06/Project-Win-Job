1c27577cb6ce0043fa2f9a963f4efacc03a0d844
Le Truong Son Dev <sonltute@gmail.com>
2024-05-05 08:51:58 +0700

    update

--- JobHub/Program.cs
+++ JobHub/Program.cs
@@ -16,7 +16,7 @@ namespace JobHub
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FMainCompany());
+            Application.Run(new Fmain());
         }
     }
 }
